4e4b205d1b82b860d3ceb75c5cb94ef50996dfd5
Jason Davies <jason@jasondavies.com>
2011-08-25 18:37:59 +0100

    Remove cruft from LOESS test.

--- science.js
+++ science.js
@@ -1,4 +1,4 @@
-(function(){science = {version: "1.3.1"}; // semver
+(function(){science = {version: "1.3.2"}; // semver
 science.functor = function(v) {
   return typeof v === "function" ? v : function() { return v; };
 };
